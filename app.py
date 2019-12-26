fc5743f5cd4546f03e894bb5b0dd6800743def2d
Nidhal Arouri <arrouurii@gmail.com>
2019-12-26 02:14:12 +0100

    Add utils containing a function that returns distance between two branchs

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+from models.Branch import Branch
 from models.Driver import Driver
 from models.Truck import Truck
 
 d = Driver("ccccc", "dddddd")
 c = Truck("eeeee", d)
+o = Branch(name="1er arr", longitude=48.864031, latitude=2.330943)
+a = Branch(name="2er arr", longitude=48.867705, latitude=2.343533)
 
-print(c.getDriver().getFullname())
+
+e = (1,1)
+f = (2, 1)
+
+print(o.getDistanceTo(a))
---
+++ utils/utils.py
@@ -0,0 +1,23 @@
+import geopy.distance as distance
+
+from models.Branch import Branch
+
+
+def distance(from_branch, destination_branch):
+    # verify whether we have got Branch instances as args
+    if not (isinstance(from_branch, Branch) and isinstance(destination_branch, Branch)):
+        raise Exception('Argument(s) not of type Branch')
+    from_branch_position = from_branch.getPosition()
+    destination_branch_position = destination_branch.getPosition()
+    # verify whether we do have not none 2 coordinates for each branch
+    if not ((
+                        (len(destination_branch_position) + len(from_branch_position) == 4 and (
+                        destination_branch_position[0])) and (
+                            destination_branch_position[1])) and (from_branch_position[0])) or not (from_branch_position[1]):
+        raise Exception('Missing postion on destination')
+
+    # distance calculated using vincenty see : https://en.wikipedia.org/wiki/Vincenty%27s_formulae
+    return distance.vincenty(
+        from_branch.getPosition(),
+        destination_branch.getPosition()
+    ).km
